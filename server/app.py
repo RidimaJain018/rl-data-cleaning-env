3a8b86e378393150b7e38aee98c315433688b56a
Ridima Jain <ridimajain018@gmail.com>
2026-04-04 22:05:03 +0530

    add correct server/app.py

---
+++ server/app.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import uvicorn
+
+def main():
+    import app as application
+    uvicorn.run(application.app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
